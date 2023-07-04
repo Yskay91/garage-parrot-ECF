4c13f176c3b6b0254deee352439f59494213920f
Yskay91 <jessica.eisele@protonmail.com>
2023-07-04 11:31:53 +0200

    Remise à zéro de la base de données

--- migrations/Version20230704071630.php
+++ migrations/Version20230704071630.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230630110435 extends AbstractMigration
+final class Version20230704071630 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230630110435 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE garage (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, adresse VARCHAR(255) NOT NULL, zip_code VARCHAR(10) NOT NULL, city VARCHAR(255) NOT NULL, phone VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cars DROP reference');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE garage');
+        $this->addSql('ALTER TABLE cars ADD reference VARCHAR(10) NOT NULL');
     }
 }
