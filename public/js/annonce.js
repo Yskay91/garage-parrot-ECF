1cd66abb31985edbce8045de7b0c34a3d22ac3e4
Yskay91 <jessica.eisele@protonmail.com>
2023-07-13 10:41:41 +0200

    Remise en ordre des dossiers et de la bdd, ajout du bundle apache-pack

---
+++ public/js/annonce.js
@@ -0,0 +1,14 @@
+
+console.log('coucou');
+
+
+$(document).ready(function () {
+  $('.modal').modal();
+  $('.modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Bouton qui déclenche la modal
+    var sujet = button.data('whatever'); // Extraire les informations des attributs data-*
+    var modal = $(this);
+    modal.find('.modal-title').text(sujet);
+    modal.find('.modal-body input').val('Demande d\'information concernant l\'annonce ' + sujet);
+  });
+});
